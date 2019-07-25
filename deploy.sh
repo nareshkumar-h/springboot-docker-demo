26a589fe1cbe785e21f82e27af3d4576a2b788b3
nareshkumar-h <nareshkumarh@live.com>
2019-07-25 14:39:09 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 echo "Stop Existing Spring Boot APP ";
-pid=$(lsof -i:9002 -t);
+pid=$(lsof -i:9002 -t);kill -TERM $pid;
 echo "PID - $pid"
-kill -TERM $pid;
 echo "Start Spring Boot App"
 nohup java -jar target/docker-demo-0.0.1-SNAPSHOT.jar --server.port=9002 & 
 
